3102b86238a712d0112e58c1b3373738e1a0e617
evreekum <e.v.reekum@gmail.com>
2022-05-01 18:00:07 +0200

    finished-variables-questions

--- variables.js
+++ variables.js
@@ -3,42 +3,62 @@
 // ==========================================
 
 const start = "Let's go!";
-console.log("lets go");
+console.log(start);
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+const firstname = "evelien";
+const inBetween = "van";
+const lastname = "reekum";
+const fullname = firstname + " " + inBetween + " " + lastname;
+console.log(fullname);
 
+//Correct:
+const name = "Evelien";
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const eyecolor = "Green";
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let age = 32;
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let job = "Student";
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const shoeSize = 38;
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
+let relationshipStatus = "Single";
 
+//Correct:
+let hasRelationship = true;
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let kidsAmount = 0;
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+console.log(name);
+console.log(eyecolor);
+console.log(age);
+console.log(job);
+console.log(shoeSize);
+console.log(relationshipStatus);
+console.log(kidsAmount);
+
+//Correct:
+console.log(name, eyecolor, age, job, shoeSize, hasRelationship, kidsAmount);
