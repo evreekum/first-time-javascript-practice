c3f11eb65fe1aa3a5d1c967aa8fa8f9b80f11575
evreekum <e.v.reekum@gmail.com>
2022-05-01 17:19:13 +0200

    finished-mathematical-operators

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -15,33 +15,47 @@ console.log(total1);
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
-
+const total2 = 7 * 52;
+console.log(total2);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
-
+const total3 = (4 + 3) * 7;
+console.log(total3);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
-
+const total4 = 36/6*2;
+console.log(total4);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
-
+const part1 = "zoet";
+const part2 = "sappig";
+const total5 = part1 + part2;
+console.log(total5);
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
-
+const firstname = "evelien";
+const inBetween = "van";
+const lastname = "reekum";
+const total6 = firstname + " " + inBetween + " " + lastname;
+console.log(total6);
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+const hometown = "Utrecht";
+const province = "Utrecht";
+const livingIn = hometown + ", " + province;
+console.log(livingIn);
